60cac67ecb53a8a7c6ddcf1e0c5e73932103707e
pietbinon <pierrebinon@icloud.com>
2017-04-13 17:06:14 -0400

    First commit with red square - Apr 13, 2017

--- Dynamics/ViewController.swift
+++ Dynamics/ViewController.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var animator: UIDynamicAnimator!
+    var gravity: UIGravityBehavior!
+    var collision: UICollisionBehavior!
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let square = UIView (frame: CGRect (x: 100, y: 100, width: 100, height: 100))
+        square.backgroundColor = UIColor.gray
+        view.addSubview (square)
+        
+        let barrier = UIView (frame: CGRect (x: 0, y: 300, width: 130, height: 20))
+        barrier.backgroundColor = UIColor.red
+        view.addSubview (barrier)
+        
+        animator = UIDynamicAnimator (referenceView: view)
+        gravity = UIGravityBehavior (items: [square])
+        animator.addBehavior(gravity)
+        
+        collision = UICollisionBehavior (items: [square])
+        
+        //Add a boundary that has the same frame as the barrier
+        collision.addBoundary(withIdentifier: "barrier" as NSCopying, for: UIBezierPath (rect: barrier.frame))
+        collision.translatesReferenceBoundsIntoBoundary = true
+        animator.addBehavior(collision)
     }
 
     override func didReceiveMemoryWarning() {
